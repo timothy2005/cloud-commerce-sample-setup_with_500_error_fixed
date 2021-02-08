7aa513c58a81618d504be0790804060b6cff02c2
I068150 <timothy.li@sap.com>
2021-02-09 00:45:14 +0800

    Update server.ts
    SSR Timeout fix

--- js-storefront/spartacusstore/server.ts
+++ js-storefront/spartacusstore/server.ts
@@ -14,7 +14,8 @@ import { APP_BASE_HREF } from '@angular/common';
 import { existsSync } from 'fs';
 import { NgExpressEngineDecorator } from '@spartacus/setup/ssr';
 
-const ngExpressEngine = NgExpressEngineDecorator.get(engine);
+//const ngExpressEngine = NgExpressEngineDecorator.get(engine);
+const ngExpressEngine = NgExpressEngineDecorator.get(engine, { timeout: 0 });
 
 // The Express app is exported so that it can be used by serverless Functions.
 export function app() {
